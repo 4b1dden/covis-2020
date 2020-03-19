8a4b96fb190f8bb110f331f24506d3dfbd4fc01b
Kiko Čabala <kristiancabala@gmail.com>
2020-03-19 12:11:44 +0100

    Updates

---
+++ precache-manifest.0661d8faec6b9e65b3db5a6c3c534ff5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d39676aca78e9e293b93d09608ca20de",
+    "url": "/covis-2020/index.html"
+  },
+  {
+    "revision": "7b5a713466335a00f95b",
+    "url": "/covis-2020/static/css/2.59c6c6bb.chunk.css"
+  },
+  {
+    "revision": "f804774ca7c61be6015d",
+    "url": "/covis-2020/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7b5a713466335a00f95b",
+    "url": "/covis-2020/static/js/2.c720c058.chunk.js"
+  },
+  {
+    "revision": "41b0c3da6a8866c17968078c7c246d41",
+    "url": "/covis-2020/static/js/2.c720c058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f804774ca7c61be6015d",
+    "url": "/covis-2020/static/js/main.57fca8c4.chunk.js"
+  },
+  {
+    "revision": "0fdda9c3735bb01edc65",
+    "url": "/covis-2020/static/js/runtime-main.ea0e90ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covis-2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
